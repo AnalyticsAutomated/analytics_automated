84d1914ed55f271936eb02e94ee92f2d211eb4ac
Daniel Buchan <clone@myself.com>
2015-07-08 16:50:36 +0100

    tidy tests

--- analytics_automated/test_api.py
+++ analytics_automated/test_api.py
@@ -74,7 +74,8 @@ class SubmissionDetailTests(APITestCase):
         response = self.client.get(reverse('submissionDetail',
                                            args=[s1.UUID, ]) + ".json")
         self.assertEqual(response.status_code, 200)
-        test_data = '{"submission_name":"submission_1","UUID":"%s","state":"Submitted"}' % s1.UUID
+        test_data = '{"submission_name":"submission_1","UUID":"%s"' \
+                    ',"state":"Submitted"}' % s1.UUID
         self.assertEqual(response.content.decode("utf-8"), test_data)
 
     @patch('builtins.eval', return_value=True)
