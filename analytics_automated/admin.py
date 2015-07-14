34f839c1d97c319d93502ee93680eb2d340d5b3a
Daniel Buchan <clone@myself.com>
2015-07-14 11:15:52 +0100

    Minor form refactor

--- analytics_automated/admin.py
+++ analytics_automated/admin.py
@@ -6,37 +6,12 @@ from django.core.exceptions import ValidationError
 
 from .models import Backend, Job, Task, Step, Parameter, Result, Validator
 from .models import Submission
+from .forms import *
 
 
 class ParameterInline(admin.TabularInline):
     model = Parameter
     extra = 3
-
-
-class ValidatorForm(BaseInlineFormSet):
-
-    class Meta:
-        model = Validator
-
-    '''
-       Validate formset data here
-    '''
-    def clean(self):
-        re_string = ''
-        validation_type = ''
-        for formset in self.cleaned_data:
-            re_string = formset['re_string']
-            validation_type = formset['validation_type']
-            if validation_type == Validator.REGEX:
-                is_valid = False
-                try:
-                    re.compile(re_string)
-                    is_valid = True
-                except re.error:
-                    is_valid = False
-                if is_valid is False:
-                    raise(ValidationError("REGULAR EXPRESSION IS NOT VALID: " +
-                                          re_string))
 
 
 class ValidatorInline(admin.TabularInline):
--- analytics_automated/forms.py
+++ analytics_automated/forms.py
@@ -1,5 +1,9 @@
+import re
 from django import forms
-from .models import Submission, Job
+from django.forms.models import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+
+from .models import Submission, Job, Validator
 
 
 class SubmissionForm(forms.ModelForm):
@@ -13,3 +17,29 @@ class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ('name',)
+
+
+class ValidatorForm(BaseInlineFormSet):
+
+    class Meta:
+        model = Validator
+
+    '''
+       Validate formset data here
+    '''
+    def clean(self):
+        re_string = ''
+        validation_type = ''
+        for formset in self.cleaned_data:
+            re_string = formset['re_string']
+            validation_type = formset['validation_type']
+            if validation_type == Validator.REGEX:
+                is_valid = False
+                try:
+                    re.compile(re_string)
+                    is_valid = True
+                except re.error:
+                    is_valid = False
+                if is_valid is False:
+                    raise(ValidationError("REGULAR EXPRESSION IS NOT VALID: " +
+                                          re_string))
