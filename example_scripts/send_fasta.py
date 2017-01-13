dc9859076cc7b2b691696bab7038833f498b825d
Daneil Buchan <clone@myself.com>
2017-01-13 15:35:09 +0000

    added task fields for controlling how jobs terminate when tasks do not produce output or raise a specific exit status

--- example_scripts/send_fasta.py
+++ example_scripts/send_fasta.py
@@ -6,7 +6,7 @@ import json
 url = 'http://127.0.0.1:8000/analytics_automated/submission.json'
 
 payload = {'input_data': ('prot.txt', open('../static/files/prot.txt', 'rb'))}
-data = {'job': 'disopred',
+data = {'job': 'domserf',
         'submission_name': 'test',
         'email': 'daniel.buchan@ucl.ac.uk'}
 r = requests.post(url, data=data, files=payload)
