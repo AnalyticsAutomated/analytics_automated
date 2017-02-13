f7d447bc8701963fc8ee95bdc4e2172d005c1b56
Daneil Buchan <clone@myself.com>
2017-02-13 14:28:09 +0000

    updated mac script for redos

--- utility_scripts/spawn_dev_processes_mac.sh
+++ utility_scripts/spawn_dev_processes_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-osascript -e 'tell application "Terminal"' -e 'do script with command "rabbitmq-server"' -e 'end tell'
+osascript -e 'tell application "Terminal"' -e 'do script with command "redis-server"' -e 'end tell'
 osascript -e 'tell application "Terminal"' -e 'do script with command "workon analytics_automated; cd ~/Code/analytics_automated; python manage.py runserver --settings=analytics_automated_project.settings.dev"' -e 'end tell'
 osascript -e 'tell application "Terminal"' -e 'do script with command "workon analytics_automated; cd ~/Code/analytics_automated; celery --app=analytics_automated_project.celery:app worker --loglevel=INFO -Q low_localhost,localhost,high_localhost,celery"' -e 'end tell'
