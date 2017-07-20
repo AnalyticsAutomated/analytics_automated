685c7465659b62bea31bea4501b6cebf67c49bef
Daneil Buchan <clone@myself.com>
2017-07-20 12:08:51 +0100

    updated other broken django packages

--- analytics_automated_project/settings/base.py
+++ analytics_automated_project/settings/base.py
@@ -62,7 +62,7 @@ EMAIL_MESSAGE_STRING = 'Your analysis is complete.\nYou can retrieve the ' \
                        'submission/'
 
 # Celery Settings
-# CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 # CELERY_RESULT_BACKEND = 'amqp'
 # CELERY_TIMEZONE = 'Europe/London'
 # CELERY_ACCEPT_CONTENT = ['json']
