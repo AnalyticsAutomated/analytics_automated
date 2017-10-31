e9ec0d37f6afc42bae157fa2467944a67530b4c0
Daneil Buchan <clone@myself.com>
2017-10-31 12:30:38 +0000

    updated URL pathing

--- analytics_automated/serializers.py
+++ analytics_automated/serializers.py
@@ -11,7 +11,7 @@ class ResultSerializer (serializers.ModelSerializer):
         fields = ('task', 'name', 'message', 'step', 'data_url')
 
     def get_data_url(self, obj):
-        return(str(obj.result_data).split("analytics_automated", 1)[1])
+        return(obj.result_data.name.split("analytics_automated", 1)[1])
 
 
 class SubmissionInputSerializer (serializers.ModelSerializer):
