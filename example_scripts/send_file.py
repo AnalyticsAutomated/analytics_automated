57c4431e3b3bfa1cda24a38751cf71ee38a8fd84
SarahWooller <>
2021-05-13 12:53:33 +0100

    changes to enable change to Django3

--- example_scripts/send_file.py
+++ example_scripts/send_file.py
@@ -9,13 +9,13 @@ import json
 # url = 'http://127.0.0.1:8000/analytics_automated/submission/32.json'
 # r = requests.get(url)
 
-url = 'http://127.0.0.1:8000/analytics_automated/submission/.json'
+url = 'http://127.0.0.1:8000/analytics_automated/submission.json'
 # payload = {'job': 'job1', 'submission_name': 'test', 'email': 'a@b.com'}
 # r = requests.post(url, json=json.dumps(payload))
 
 payload = {'input_data': ('input.txt', open('../static/files/file1.txt',
                           'rb'))}
 data = {'job': 'tmp_count', 'submission_name': 'test',
-        'email': 'daniel.buchan@ucl.ac.uk'}
+        'email': 'swooller@ucl.ac.uk'}
 r = requests.post(url, data=data, files=payload)
 print(r.text)
