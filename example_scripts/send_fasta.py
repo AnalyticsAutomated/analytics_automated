a0b2d0d00edcad156f006a4ed96d3c3d5f125202
Daniel Buchan <clone@myself.com>
2021-07-21 11:20:32 +0100

    minor utility script update

--- example_scripts/send_fasta.py
+++ example_scripts/send_fasta.py
@@ -2,10 +2,11 @@ import os
 import requests
 import json
 
-url = 'http://bioinf.cs.ucl.ac.uk/psipred_beta/api/submission.json'
+# url = 'http://bioinf.cs.ucl.ac.uk/psipred_beta/api/submission.json'
+url = 'http://127.0.0.1:8000/analytics_automated/submission.json'
 
 payload = {'input_data': ('prot.txt', open('../submissions/files/prot.txt', 'rb'))}
-data = {'job': 'disopred',
+data = {'job': 'psipred',
         'submission_name': 'testing',
         'email': 'daniel.buchan@ucl.ac.uk', }
 r = requests.post(url, data=data, files=payload)
